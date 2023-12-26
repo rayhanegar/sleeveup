4b83fdd8e812eb2922a8a6cf6d78a2cf89136fee
hasanfadhlillah <hasanfadhlillah01@gmail.com>
2023-12-26 21:17:05 +0700

    update feedback & event page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
