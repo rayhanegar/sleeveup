2b97b97fee056fd567887ef87b580f5ba12e6efc
Rayhan Egar <rresnn@gmail.com>
2023-12-28 13:30:11 +0700

    Working menu

--- navigation.js
+++ navigation.js
@@ -1,23 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("exploreBtn").addEventListener("click", function() {
         try {
-            window.location.href = "explore.html"
+            window.location.href = "explore.html";
         } catch (error) {
             console.error("Error opening explore.html:", error);
         }
     });
+    
     document.getElementById("sleeveUpNav").addEventListener("click", function() {
         try {
-            window.location.href = "homepage.html"
+            window.location.href = "homepage.html";
         } catch (error) {
             console.error("Error opening homepage.html:", error);
         }
     });
+
     document.getElementById("perusahaanBtn").addEventListener("click", function() {
         try {
-            window.location.href = "company.html"
+            window.location.href = "company.html";
         } catch (error) {
-            console.error("Error opening company.html:", error)
+            console.error("Error opening company.html:", error);
         }
     });
+
+    document.getElementById("sinaustudioBtn").addEventListener("click", function() {
+        try {
+            window.location.href = "sinau.html";
+        } catch (error) {
+            console.error("Error opening sinau.html:", error);
+        }
+    })
 });
