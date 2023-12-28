1e75ef013ad92ef650ca4e720d7b092ee3395903
Rayhan Egar <rresnn@gmail.com>
2023-12-28 19:14:56 +0700

    Added navigation.js

--- navigation.js
+++ navigation.js
@@ -23,11 +23,35 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    document.getElementById("cvstudioBtn").addEventListener("click", function() {
+        try {
+            window.location.href = "cvstudio.html";
+        } catch (error) {
+            console.error("Error opening cvstudio.html:", error);
+        }
+    });
+
     document.getElementById("sinaustudioBtn").addEventListener("click", function() {
         try {
             window.location.href = "sinau.html";
         } catch (error) {
             console.error("Error opening sinau.html:", error);
+        }
+    });
+
+    document.getElementById("rekomendasiBtn").addEventListener("click", function() {
+        try {
+            window.location.href = "index.html";
+        } catch (error) {
+            console.error("Error opening index.html:", error);
+        }
+    });
+
+    document.getElementById("profileBtn").addEventListener("click", function() {
+        try {
+            window.location.href = "profile.html";
+        } catch (error) {
+            console.error("Error opening profile.html:", error);
         }
     })
 });
